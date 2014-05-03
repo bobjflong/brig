8f13395792d5d002cea7f5a5e291cc64d13e80b8
Bob Long <robertjflong@gmail.com>
2014-05-03 14:32:25 +0100

    Bumping page size, and removing LetMany (now stdlib)

--- macros/macros.rb
+++ macros/macros.rb
@@ -143,21 +143,10 @@ module BrigMacros
     end
   end
 
-  def self.let_many
-    LispMacro.new 'LetMany' do |ast|
-      res = ""
-      ast.drop(1).each do |let|
-        res += [:Let, let.first, let.drop(1).first].to_sxp
-      end
-      res
-    end
-  end
-
   def self.macros
     MacroList.new [BrigMacros.display_version, BrigMacros.cat, BrigMacros.arg, BrigMacros.case, BrigMacros.eq,BrigMacros.null_warning, BrigMacros.create_file,
     BrigMacros.empty_list, BrigMacros.not, BrigMacros.newline,
-    BrigMacros.get_entered_name, BrigMacros.fill_tag, BrigMacros.index_location,
-    BrigMacros.let_many]
+    BrigMacros.get_entered_name, BrigMacros.fill_tag, BrigMacros.index_location]
   end
 
 end
